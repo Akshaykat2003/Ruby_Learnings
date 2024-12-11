39095aee828bd469fb2f082eee550e1d13042d43
akshay <akshaykatoch38@gmail.com>
2024-12-11 09:48:22 +0530

    [Akshay] MOD:Done Some Minor Syntax Chnages

--- Practice6.rb
+++ Practice6.rb
@@ -4,14 +4,16 @@
 #Write a Ruby program to process an array of names. 
 #If a specific name (e.g., "exit") is found, 
 #immediately stop the processing using throw and print a message indicating the name was found.
+
 names = gets.split
 inputName = gets.chomp
 
 catch(:name_found) do
-  names.each.with_index do |name ,index|
-    if name == inputName
-      puts "Name found at position #{index+1}"
-      throw(:name_found)
+  names.each_with_index do |names,index| 
+    if names == inputName
+     puts "value is found at position : #{index+1}"
+     throw(:name_found)
     end
   end
+ puts "Not found"
 end
