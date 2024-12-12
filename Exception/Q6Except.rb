72c238f93b06de8c17ac3f3a0ea9e0e3989ed935
akshay <akshaykatoch38@gmail.com>
2024-12-12 09:37:50 +0530

    [Akshay] MOD :Done some minor changes

--- Exception/Q6Except.rb
+++ Exception/Q6Except.rb
@@ -1,7 +1,8 @@
 # Problem Statement: Write a Ruby program that opens a file for writing, 
 #writes some content, and ensures the file is closed properly even if an exception occurs during the writing process.
 
-file_path = "non_existent_directory/text.rb"
+# file_path = "non_existent_directory/text.rb"
+file_path = "Exception/text.rb"
 begin
   
 file =  File.open(file_path,"w")
@@ -12,8 +13,12 @@ puts "File Writing is completed"
 
 rescue Errno::ENOENT => e
   puts "File not found: #{e.message}"
+
+  
 rescue => e
   puts "#{e.message}"
+
+
 ensure
   if file
     file.close
