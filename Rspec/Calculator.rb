cc3647ad64ae56699ce3f25f7d0db049a3d30b98
akshay <akshaykatoch38@gmail.com>
2024-12-13 15:24:34 +0530

    [Akshay] ADD :Added calculator file with test cases

---
+++ Rspec/Calculator.rb
@@ -0,0 +1,19 @@
+
+class Calculator
+  def add(a, b)
+    a + b
+  end
+
+  def subtract(a, b)
+    a - b
+  end
+
+  def multiply(a, b)
+    a * b
+  end
+
+  def divide(a, b)
+    raise ZeroDivisionError, "Division by zero is not allowed" if b.zero?
+    a / b.to_f 
+  end
+end
